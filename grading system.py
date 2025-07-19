0986003767eaa5e1e3c855eaea602e30f7b18aa3
donxx657 <sylvesterchiboy57@gmail.com>
2025-07-19 18:24:08 +0100

    Create grading system.py

---
+++ grading system.py
@@ -0,0 +1,14 @@
+score = int(input("Enter your score (0–100): "))
+
+if score >= 70:
+    print("Grade: A")
+elif score >= 60:
+    print("Grade: B")
+elif score >= 50:
+    print("Grade: C")
+elif score >= 45:
+    print("Grade: D")
+elif score >= 40:
+    print("Grade: E")
+else:
+    print("Grade: F")
